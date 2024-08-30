59a3202096d61f6862913cd9893934bc26400eb3
igorpereirag <igorpereira1501@gmail.com>
2024-08-30 20:40:00 -0300

    chore: add inport models toys

--- restful01/toys/admin.py
+++ restful01/toys/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
-
+from toys.models import Toy
 # Register your models here.
+
+
+
+class Toyadmin(admin.ModelAdmin):
+    list_display = [
+        
+        "name",
+        "description",
+        "release_date",
+        "toy_category",
+    ]
+    search_fields=["name", "toy_category"]
+    list_filter=["toy_category","release_date","was_included_in_home"]
+    
+admin.site.register(Toy,Toyadmin)
